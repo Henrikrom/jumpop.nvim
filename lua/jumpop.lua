1477bc158e857d07013910ac5d348c64b07267db
Henrik Romnes <hbr@synergysky.com>
2025-07-14 21:33:08 +0200

    Jump over first char block found on current line

--- lua/jumpop.lua
+++ lua/jumpop.lua
@@ -2,11 +2,32 @@ local M = {}
 local config = {
     max_offset = 10,
     direction = "both",
-    jump_first_on_line = true
+    jump_first_on_line = true,
+    dev = true
+}
+
+local open_close_char_map = {
+  ["("] = ")",
+  [")"] = "(",
+  ["{"] = "}",
+  ["}"] = "{",
+  ["["] = "]",
+  ["]"] = "[",
+  ['"'] = '"',
+  ["'"] = "'",
+  ["`"] = "`",
 }
 
 function M.setup(user_config)
     config = vim.tbl_extend("force", config, user_config or {})
+
+    if config.dev then
+        vim.keymap.set("n", "<leader>r", function()
+            vim.cmd("so")
+            vim.cmd("Lazy reload jumpop.nvim")
+        end)
+    end
+
     vim.keymap.set("n", "<leader>ci", function()
         M.ci_nearest_multiline()
     end)
@@ -26,23 +47,34 @@ function M.ci_nearest_multiline()
     end
 end
 
+function find_block(line, start_col, target_char)
+    local found_col = string.find(line:sub(start_col), target_char, 1, true)
+    if found_col then
+        local found_closing = string.find(line:sub(start_col), open_close_char_map[target_char], found_col + 1, true)
+        if found_closing then
+            return found_col, found_closing
+        end
+    end
+
+    return nil
+end
+
+
 function M.find_nearest_char(target_char)
     local current_line = vim.fn.line(".")
     local total_lines = vim.fn.line("$")
     local col_start = vim.fn.col(".")
     local current_text = vim.fn.getline(current_line)
 
-    local first_occurence_for_line = string.find(current_text:sub(col_start), target_char, 1, true)
-    if first_occurence_for_line then
-        local first_col = col_start + first_occurence_for_line - 2
+    local block_start_col, block_end_col = find_block(current_text, col_start, target_char)
+    if block_start_col then
+        local first_col = col_start + block_start_col - 2
         vim.api.nvim_win_set_cursor(0, { current_line, first_col })
 
         if config.jump_first_on_line then
-            local search_start = col_start + first_occurence_for_line
-            local second_relative = string.find(current_text:sub(search_start), target_char, 1, true)
-
-            if second_relative then
-                local second_col = search_start + second_relative - 2
+            local second_block, _ = find_block(current_text, block_end_col + 1, target_char)
+            if second_block then
+                local second_col = block_end_col + second_block - 1
                 vim.api.nvim_win_set_cursor(0, { current_line, second_col })
             end
         end
@@ -82,4 +114,13 @@ function M.find_nearest_char(target_char)
 end
 
 return M
+
+
+-- Test Text:
+-- test "test" test "test"
+-- test 'test' test 'test'
+-- test "
+-- test "test" test "
+-- test {test} test {test}
+-- test (test) test (test)
 
